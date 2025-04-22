8f536c1568aff841e911353aac45449058c83ec6
WholesomeAhki <Victor.D.Jackson@gmail.com>
2025-04-22 14:24:58 +0200

    Enable Node & Jest env in ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+// .eslintrc.js
+module.exports = {
+    env: {
+      browser: false,
+      node: true,
+      commonjs: true,
+      es2021: true,
+      jest: true
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+      ecmaVersion: 12
+    },
+    rules: {
+      // Add or customize rules here
+    }
+  };
+  
